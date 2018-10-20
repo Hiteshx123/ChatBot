01917966425b72a9bb5f40eed225bfb4da212768
Hiteshx123 <43832284+Hiteshx123@users.noreply.github.com>
2018-10-19 21:40:17 -0400

    Add files via upload

--- backBalance.java
+++ backBalance.java
@@ -15,9 +15,10 @@ class backBalance
     Set set = hm.entrySet();
     
     Scanner in = new Scanner(System.in);
-    String statement = in.nextLine();
-    double statement2 = in.nextDouble();
-    while (!statement.equalsIgnoreCase("exit"))
+    String statement = ("A");
+    double statement2 = 1;
+    
+    while (!statement.equalsIgnoreCase("specialexit"))
     {
       double savedbalance = statement2;
       double balance = 0;
@@ -33,7 +34,7 @@ class backBalance
           System.out.print("\nIncorrect, Please try again: ");
           statement = in.nextLine();
         }
-        System.out.print("What would you like to Withdraw or Deposit");
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
         statement = in.nextLine();
         if (statement.equalsIgnoreCase("Withdraw"))
         {  
@@ -41,20 +42,19 @@ class backBalance
           statement2 = in.nextDouble();
           balance = ((Double)hm.get("Hitesh")).doubleValue();
           hm.put("Hitesh", new Double(balance - statement2));
-          System.out.print("This is your final balance: " + hm.get("Hitesh"));
-          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          System.out.print("This is your final balance: " + hm.get("Hitesh") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
           statement = in.nextLine();
           if (statement.equalsIgnoreCase("Confirm"))
           {
-               statement = ("exit");
+               System.out.print("Thank you");
           }
           else
           {
                hm.put("Hitesh", new Double(balance + savedbalance));
-               statement = ("exit");
+               System.out.print("Thank you");
           }
         }
-
         if (statement.equalsIgnoreCase("Deposit"))
         {
           System.out.print("How huch would you like to Deposit: ");
@@ -63,18 +63,25 @@ class backBalance
           hm.put("Hitesh", new Double(balance + statement2));
           System.out.print("This is your final balance: " + hm.get("Hitesh"));
           System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
           statement = in.nextLine();
            if (statement.equalsIgnoreCase("Confirm"))
           {
-               statement = ("exit");
+                 System.out.print("Thank you");
           }
           else
           {
                hm.put("Hitesh", new Double(balance - savedbalance));
-               statement = ("exit");
+                 System.out.print("\nThank you");
           }
         }
-         
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Hitesh"));
+          }     
+          
+        
+      } 
          
            
         
@@ -87,64 +94,352 @@ class backBalance
           System.out.print("\nIncorrect, Please try again: ");
           statement = in.nextLine();
         }
-        System.out.print("What would you like to do\n Withdraw or Deposit");
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
+        statement = in.nextLine();
         if (statement.equalsIgnoreCase("Withdraw"))
         {  
           System.out.print("How huch would you like to Withdraw: ");
           statement2 = in.nextDouble();
           balance = ((Double)hm.get("Rohan")).doubleValue();
           hm.put("Rohan", new Double(balance - statement2));
-           System.out.print("This is your final balance: " + hm.get("Rohan"));
-          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          System.out.print("This is your final balance: " + hm.get("Rohan") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
           statement = in.nextLine();
           if (statement.equalsIgnoreCase("Confirm"))
           {
-               statement = ("exit");
+               System.out.print("Thank you");
           }
           else
           {
                hm.put("Rohan", new Double(balance + savedbalance));
-               statement = ("exit");
+               System.out.print("Thank you");
           }
         }
 
         if (statement.equalsIgnoreCase("Deposit"))
         {
-           System.out.print("How huch would you like to Deposit: ");
-          statement = in.nextLine();
+          System.out.print("How huch would you like to Deposit: ");
+          statement2 = in.nextDouble();
           balance = ((Double)hm.get("Rohan")).doubleValue();
-          hm.put("Rohan", new Double(balance + statement));
+          hm.put("Rohan", new Double(balance + statement2));
           System.out.print("This is your final balance: " + hm.get("Rohan"));
           System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
           statement = in.nextLine();
            if (statement.equalsIgnoreCase("Confirm"))
           {
-               statement = ("exit");
+                 System.out.print("Thank you");
           }
           else
           {
                hm.put("Rohan", new Double(balance - savedbalance));
-               statement = ("exit");
+                 System.out.print("\nThank you");
           }
+        }
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Rohan"));
+          }    
+        
       }
       else if (statement.equalsIgnoreCase("Rohit"))
       {
+          System.out.print("\nPlease input your pin: ");
+        statement = in.nextLine();
+        while (!statement.equals("hitech"))
+        {
+          System.out.print("\nIncorrect, Please try again: ");
+          statement = in.nextLine();
+        }
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
+        statement = in.nextLine();
+        if (statement.equalsIgnoreCase("Withdraw"))
+        {  
+          System.out.print("How huch would you like to Withdraw: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Rohit")).doubleValue();
+          hm.put("Rohit", new Double(balance - statement2));
+          System.out.print("This is your final balance: " + hm.get("Rohit") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+          if (statement.equalsIgnoreCase("Confirm"))
+          {
+               System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Rohit", new Double(balance + savedbalance));
+               System.out.print("Thank you");
+          }
+        }
+
+        if (statement.equalsIgnoreCase("Deposit"))
+        {
+          System.out.print("How huch would you like to Deposit: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Rohit")).doubleValue();
+          hm.put("Rohit", new Double(balance + statement2));
+          System.out.print("This is your final balance: " + hm.get("Rohit"));
+          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+           if (statement.equalsIgnoreCase("Confirm"))
+          {
+                 System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Rohit", new Double(balance - savedbalance));
+                 System.out.print("\nThank you");
+          }
+        }
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Rohit"));
+          }    
+        
       }
       else if (statement.equalsIgnoreCase("Mohit"))
       {
+        System.out.print("\nPlease input your pin: ");
+        statement = in.nextLine();
+        while (!statement.equals("hitech"))
+        {
+          System.out.print("\nIncorrect, Please try again: ");
+          statement = in.nextLine();
+        }
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
+        statement = in.nextLine();
+        if (statement.equalsIgnoreCase("Withdraw"))
+        {  
+          System.out.print("How huch would you like to Withdraw: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Mohit")).doubleValue();
+          hm.put("Mohit", new Double(balance - statement2));
+          System.out.print("This is your final balance: " + hm.get("Mohit") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+          if (statement.equalsIgnoreCase("Confirm"))
+          {
+               System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Mohit", new Double(balance + savedbalance));
+               System.out.print("Thank you");
+          }
+        }
+
+        if (statement.equalsIgnoreCase("Deposit"))
+        {
+          System.out.print("How huch would you like to Deposit: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Mohit")).doubleValue();
+          hm.put("Mohit", new Double(balance + statement2));
+          System.out.print("This is your final balance: " + hm.get("Mohit"));
+          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+           if (statement.equalsIgnoreCase("Confirm"))
+          {
+                 System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Mohit", new Double(balance - savedbalance));
+                 System.out.print("\nThank you");
+          }
+        }
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Mohit"));
+          }    
+        
+
+
       }
       else if (statement.equalsIgnoreCase("Ashish"))
       {
+        System.out.print("\nPlease input your pin: ");
+        statement = in.nextLine();
+        while (!statement.equals("hitech"))
+        {
+          System.out.print("\nIncorrect, Please try again: ");
+          statement = in.nextLine();
+        }
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
+        statement = in.nextLine();
+        if (statement.equalsIgnoreCase("Withdraw"))
+        {  
+          System.out.print("How huch would you like to Withdraw: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Ashish")).doubleValue();
+          hm.put("Ashish", new Double(balance - statement2));
+          System.out.print("This is your final balance: " + hm.get("Ashish") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+          if (statement.equalsIgnoreCase("Confirm"))
+          {
+               System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Ashish", new Double(balance + savedbalance));
+               System.out.print("Thank you");
+          }
+        }
+
+        if (statement.equalsIgnoreCase("Deposit"))
+        {
+          System.out.print("How huch would you like to Deposit: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Ashish")).doubleValue();
+          hm.put("Ashish", new Double(balance + statement2));
+          System.out.print("This is your final balance: " + hm.get("Ashish"));
+          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+           if (statement.equalsIgnoreCase("Confirm"))
+          {
+                 System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Ashish", new Double(balance - savedbalance));
+                 System.out.print("\nThank you");
+          }
+        }
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Ashish"));
+          }    
+        
+
+
       }
       else if (statement.equalsIgnoreCase("Khariwal"))
       {
+        System.out.print("\nPlease input your pin: ");
+        statement = in.nextLine();
+        while (!statement.equals("hitech"))
+        {
+          System.out.print("\nIncorrect, Please try again: ");
+          statement = in.nextLine();
+        }
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
+        statement = in.nextLine();
+        if (statement.equalsIgnoreCase("Withdraw"))
+        {  
+          System.out.print("How huch would you like to Withdraw: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Khariwal")).doubleValue();
+          hm.put("Khariwal", new Double(balance - statement2));
+          System.out.print("This is your final balance: " + hm.get("Khariwal") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          
+          statement = in.nextLine();
+          if (statement.equalsIgnoreCase("Confirm"))
+          {
+               System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Khariwal", new Double(balance + savedbalance));
+               System.out.print("Thank you");
+          }
+        }
+
+        if (statement.equalsIgnoreCase("Deposit"))
+        {
+          System.out.print("How huch would you like to Deposit: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Khariwal")).doubleValue();
+          hm.put("Khariwal", new Double(balance + statement2));
+          System.out.print("This is your final balance: " + hm.get("Khariwal"));
+          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+           if (statement.equalsIgnoreCase("Confirm"))
+          {
+                 System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Khariwal", new Double(balance - savedbalance));
+                 System.out.print("\nThank you");
+          }
+        }
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Khariwal"));
+          }    
+        
+
+
       }
       else if (statement.equalsIgnoreCase("Pankaj"))
       {
+        System.out.print("\nPlease input your pin: ");
+        statement = in.nextLine();
+        while (!statement.equals("hitech"))
+        {
+          System.out.print("\nIncorrect, Please try again: ");
+          statement = in.nextLine();
+        }
+        System.out.print("What would you like to Withdraw or Deposit or Check Balance");
+        statement = in.nextLine();
+        if (statement.equalsIgnoreCase("Withdraw"))
+        {  
+          System.out.print("How huch would you like to Withdraw: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Pankaj")).doubleValue();
+          hm.put("Pankaj", new Double(balance - statement2));
+          System.out.print("This is your final balance: " + hm.get("Pankaj") + " \nPlease enter Confirm if you are Happy with Your transaction");
+          
+          statement = in.nextLine();
+          if (statement.equalsIgnoreCase("Confirm"))
+          {
+               System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Pankaj", new Double(balance + savedbalance));
+               System.out.print("Thank you");
+          }
+        }
+
+        if (statement.equalsIgnoreCase("Deposit"))
+        {
+          System.out.print("How huch would you like to Deposit: ");
+          statement2 = in.nextDouble();
+          balance = ((Double)hm.get("Pankaj")).doubleValue();
+          hm.put("Pankaj", new Double(balance + statement2));
+          System.out.print("This is your final balance: " + hm.get("Pankaj"));
+          System.out.print("\nPlease enter Confirm if you are Happy with Your transaction");
+          in.nextLine();
+          statement = in.nextLine();
+           if (statement.equalsIgnoreCase("Confirm"))
+          {
+                 System.out.print("Thank you");
+          }
+          else
+          {
+               hm.put("Pankaj", new Double(balance - savedbalance));
+                 System.out.print("\nThank you");
+          }
+        }
+          if (statement.equalsIgnoreCase("Check Balance"))
+          {
+            System.out.print("Your current balance is " + hm.get("Pankaj"));
+          }    
+        
+
+
       }
     } 
   }
-} 
 }
-}
+
+
+
+
   
